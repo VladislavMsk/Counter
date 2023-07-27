21b2f00c34ffb612ce63243bcf1369a82e641224
Vladislav Tudos <vladislav.grand@gmail.com>
2023-07-27 14:38:09 +0300

    ДЗ по 3 спринту. Версия: Усложненная.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0 //Счетчик
+    var historyText = "История изменений:"
+    var date: Date = Date()
+    
+    func Upd(){
+        date = Date()
+        Count.text = String(count)
+        HistoryCount.text = historyText
+    }
+    
+    
+    @IBOutlet weak var HistoryCount: UITextView!
+    @IBOutlet weak var Count: UILabel!
+    @IBOutlet weak var AddCount: UIButton!
+    @IBOutlet weak var MinusCount: UIButton!
+    @IBOutlet weak var ResetCount: UIButton!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AddCount.backgroundColor = UIColor.red
+        MinusCount.backgroundColor = UIColor.blue
+        HistoryCount.isScrollEnabled = true
+        
     }
-
-
+    
+    @IBAction func AddCountTap(_ sender: Any) {
+        count += 1
+        historyText += "\n [\(date)]: значение изменено на +1"
+        Upd()
+        
+    }
+    
+    @IBAction func MinusCountTap(_ sender: Any) {
+        
+        count -= 1
+        if (count < 0){
+            historyText += "\n [\(date)]: попытка уменьшить значение счётчика ниже 0"
+            count += 1
+            Upd()
+        } else{
+            historyText += "\n [\(date)]: значение изменено на -1"
+            Upd()
+        }
+    }
+    
+    @IBAction func ResetCountTap(_ sender: Any) {
+    
+        count = 0
+        historyText += "\n [\(date)]: значение сброшено"
+        Upd()
+    }
+    
+    
 }
 
